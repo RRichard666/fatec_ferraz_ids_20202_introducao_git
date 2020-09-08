18ba5a09fa11a73a3d619ddff9bfb5d69937b041
Richard Santos <richardsantos118@hotmail.com>
2020-09-08 10:50:00 -0300

    Já faz soma e Subtracao

--- aula.py
+++ aula.py
@@ -1,4 +1,10 @@
+print('Soma ou Subtraçcao')
+op = int(input('1 soma 2 subtracao' ))
 x = int(input('Digite um valor: '))
 y = int(input('Digite outro valor: '))
-resultado = x + y
-print(resultado)
+res = 0
+if op == 1:
+	res = x + y
+else:
+	res = x - y
+print(res)
